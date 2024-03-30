194e5137c077833b4a555f55d6a0776fdabb6d0e
jaseongKim <jaseong4281@gmail.com>
2024-03-31 01:57:17 +0900

    feat: signup 유효성 검사

--- src/main/java/com/sport/service/APIUserServiceImpl.java
+++ src/main/java/com/sport/service/APIUserServiceImpl.java
@@ -10,6 +10,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Log4j2
 @Service
 @RequiredArgsConstructor
@@ -22,7 +27,25 @@ public class APIUserServiceImpl implements APIUserService {
     private final PasswordEncoder passwordEncoder;
 
     @Override
-    public void signup(APIUserJoinDTO apiUserJoinDTO)  {
+    public Map<String,String> signup(APIUserJoinDTO apiUserJoinDTO)  {
+        Map<String, String> map = new HashMap<>();
+
+        if(apiUserJoinDTO.getMpw().length() >= 10) {
+            map = Map.of("success", "false", "data", "비밀번호 오류");
+            return map;
+        } else if (apiUserJoinDTO.getNickName().length() >=10) {
+            map = Map.of("success", "false", "data", "닉네임 오류");
+            return map;
+        } else if (apiUserRepository.existsBynickName(apiUserJoinDTO.getNickName())) {
+            map = Map.of("success", "false", "data", "닉네임 중복");
+            return map;
+        } else if (apiUserRepository.existsByemail(apiUserJoinDTO.getEmail())) {
+            map = Map.of("success", "false", "data", "이메일 중복");
+            return map;
+        } else if (!emailValidator(apiUserJoinDTO.getEmail())) {
+            map = Map.of("success", "false", "data", "이메일 오류");
+            return map;
+        }
 
         APIUser apiUser = modelMapper.map(apiUserJoinDTO, APIUser.class);
         apiUser.changeMpw(passwordEncoder.encode(apiUserJoinDTO.getMpw()));
@@ -33,5 +56,21 @@ public class APIUserServiceImpl implements APIUserService {
         log.info(apiUser.getRoleSet());
 
         apiUserRepository.save(apiUser);
+
+        map = Map.of("success", "true");
+
+        return map;
+    }
+
+    public boolean emailValidator(String email) {
+        String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+        Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+        if (email == null) {
+            return false;
+        }
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
+        return matcher.matches();
     }
 }
